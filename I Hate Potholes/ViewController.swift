ea67793496629f8941e2598526e883f09feaafbf
Eric Bates <ericbates@erics-imac.local>
2018-10-13 17:39:58 -0400

    Just a rough idea of what I wanna do

--- I Hate Potholes/ViewController.swift
+++ I Hate Potholes/ViewController.swift
@@ -8,8 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    @IBOutlet weak var photosButton: UIButton!
+    
+    @IBAction func takeImage(_ sender: Any) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        
+        image.sourceType = UIImagePickerController.SourceType.camera
+        
+        image.cameraDevice = .rear
+        
+        image.allowsEditing = true
+        
+        self.present(image, animated: true)
+        {
+            //After it is complete
+        }
+    }
+    
+    @IBAction func importImage(_ sender: Any) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        
+        image.allowsEditing = true
+        
+        self.present(image, animated: true)
+        {
+            //After it is complete
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        {
+            imageView.image = image
+        }
+        else
+        {
+            //Error Message
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
