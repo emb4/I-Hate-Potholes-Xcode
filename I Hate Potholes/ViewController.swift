eb7889b374ca6fd68e6848cc0817449d8cd055b9
Eric Bates <ericbates@erics-imac.local>
2018-10-13 15:19:17 -0400

    Initial Commit

---
+++ I Hate Potholes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Hate Potholes
+//
+//  Created by Eric Bates on 10/13/18.
+//  Copyright © 2018 Eric Bates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
